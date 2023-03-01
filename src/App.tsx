f1f279140fa3d27d83c0e548b8c6f829fe4eeb1b
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-03-02 01:49:02 +0300

    edit: render grid

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
 import './App.scss';
 import { Container } from './components/Container/Container';
+import { Grid } from './components/Grid/Grid';
+import { GridElement } from './components/GridElement/GreidElement';
 import { InfoBlock } from './components/InfoBlock/InfoBlock';
 
 function App() {
+	const arr = Array(256).fill('1');
 	return (
 		<div className="App">
 			<Container>
 				<InfoBlock />
+				<Grid>
+					{arr.map((_) => (
+						<GridElement />
+					))}
+				</Grid>
 			</Container>
 		</div>
 	);
