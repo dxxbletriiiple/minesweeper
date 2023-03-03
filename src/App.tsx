ae6395458f4effeeb7fd09d89ecbea6f9dc3f672
dxxbletriiiple <dxxbletriiiple@gmail.com>
2023-03-03 22:45:31 +0300

    refactor: delete functions on App component

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,10 @@ import { Grid } from './components/Grid/Grid';
 import { GridElement } from './components/GridElement/GridElement';
 import { IGridElementProps } from './components/GridElement/GridElement.interface';
 import { RootState } from './store/store';
-import { onClick, onContextMenu } from './store/reducers';
 import './App.scss';
 
 function App() {
 	const grid = useSelector((state: RootState) => state.grid.grid);
-	const dispatch = useDispatch();
-
-	const handleClick = (id: string) => {
-		dispatch(onClick(id));
-	};
-	const handleContextMenu = (e: React.MouseEvent, id: string) => {
-		e.preventDefault();
-		dispatch(onContextMenu(id));
-	};
 
 	return (
 		<div className="App">
@@ -26,7 +16,7 @@ function App() {
 				<InfoBlock />
 				<Grid>
 					{grid.map((el: IGridElementProps) => (
-						<GridElement {...el} key={el.id} onClick={handleClick} onContextMenu={handleContextMenu} />
+						<GridElement {...el} key={el.id} />
 					))}
 				</Grid>
 			</Container>
